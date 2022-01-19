259916c7e810bab2f5c43c73172ab1a0c3b7df3b
Francois Spies <fcspies@gmail.com>
2022-01-19 10:10:38 -0500

    dogs on the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function getDogs() {
+    fetch(imgUrl)
+    .then(r=>r.json())
+    .then(data => dogPics(data))
+}
+
+function dogPics(data) {
+    console.log(data)
+    const dogImg = document.querySelector('#dog-image-container');
+    const newDog = document.createElement('img');
+    data.message.forEach(d=> {
+
+        newDog.src = d;
+        dogImg.appendChild(newDog)
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    getDogs()
+})
