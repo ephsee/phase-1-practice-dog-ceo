909ade45fead8a0ea127cebc93da26bcb7ac7df4
Kevin Gallo <Gallok731@gmail.com>
2022-01-24 15:29:23 -0500

    Completed assignment"
    exit
    .exit
    
    "

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
@@ -6,21 +6,21 @@ document.addEventListener('DOMContentLoaded', function(){
     getDogs()
     getBreeds()
 })
-
-
-
+function getDogs() {
+    fetch(imgUrl)
+    .then(r=>r.json())
+    .then(data => dogPics(data))
+}
 function getBreeds(){
     fetch(breedUrl)
     .then(r=>r.json())
     .then(data => dogList(data))
 }
+const listDog = document.getElementById("dog-breeds")
 
 function dogList(data){
-    //console.log(Object.keys(data.message))
     const listOfDogs = Object.keys(data.message)
     listOfDogs.forEach(d => {
-
-        const listDog = document.getElementById("dog-breeds")
         const newList = document.createElement("li")
         newList.className = 'dogLi'
         newList.innerText = d
@@ -29,36 +29,35 @@ function dogList(data){
             console.log(e)
             newList.style.color = "red";
         })
-        document.querySelector("select").addEventListener("change", () => 
-        {
-            const dogs = document.getElementById("dog-breeds")
-            dogs.removeChild(dogs.childNodes[0])
-            if(document.querySelector("select").value === "a")
-            {
-
-                // for (breed of listOfDogs){
-                //     if()
-                // }
-            }else if(document.querySelector("select").value === "b")
-            {
-                
-            }else if(document.querySelector("select").value === "c")
-            {
-                
-            }else if(document.querySelector("select").value === "d")
-            {
-                
+        const menu = document.querySelector('select')
+        menu.addEventListener("change", () => {
+            if(menu.value === 'a'){
+                if(d[0] != 'a'){
+                    newList.textContent = d
+                    newList.style.display =  'none'
+                }else{
+                    newList.textContent = d
+                }
+            }else if(menu.value === 'b'){
+                if(d[0] != 'b'){
+                newList.remove(newList.textContent === d)
+                }
+            }
+            else if(menu.value === 'c'){
+                if(d[0] != 'c'){
+                newList.remove(newList.textContent === d)
+                }
+            }else if(menu.value === 'd'){
+                if(d[0] != 'd'){
+                    newList.textContent = d
+                    newList.style.display =  'none'
+                }else{
+                    newList.textContent = d
+                }
             }
         })
     })
 }
-
-function getDogs() {
-    fetch(imgUrl)
-    .then(r=>r.json())
-    .then(data => dogPics(data))
-}
-
 function dogPics(data) {
     data.message.forEach(d=> {
         const dogImg = document.querySelector('#dog-image-container');
